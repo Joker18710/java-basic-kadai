0252abac525dc1e42b413ac6c46dc9683dc99c76
Joker18710 <146701660+Joker18710@users.noreply.github.com>
2023-12-07 11:49:50 +0900

    Add files via upload

---
+++ kadai_014/CarExec_Chapter14.java
@@ -0,0 +1,13 @@
+package kadai_014;
+
+public class CarExec_Chapter14 {
+	public static void main(String[] args) {
+		Car_Capter14 gear = new Car_Capter14();
+		Car_Capter14 speed = new Car_Capter14();
+		
+		gear.gearChange(3);
+		
+		speed.run();
+
+}
+}
